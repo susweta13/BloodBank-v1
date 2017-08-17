dcf1bc98a864e3eca7df3fdb790d4f9d8ca24ee8
Aninda <basu.anindabasu.aninda@gmail.com>
2017-08-17 20:43:05 +0530

    app v1

---
+++ Blood_Bank_New/Blood_Bank_Application/Blood_Bank_Application/Javascripts/TitleController.js
@@ -0,0 +1,76 @@
+﻿// JavaScript source code
+(function () {
+    var app = angular.module("AngularProject");
+
+
+    var TitleController = function ($scope, $location) {
+
+
+        $scope.isRegisterDiv = false;
+        $scope.isReceiverDashboard = true;
+        $scope.isLogin = false;
+        $scope.loggedInReceiver = false;
+        $scope.donorDashboard = true;
+
+        $scope.donor = function () {
+            $scope.isDonorLogin = true;
+            $scope.donorDashboard = false;
+        }
+
+        $scope.Login = function () {
+            $scope.isLogin = true;
+            $scope.isReceiverDashboard = false;
+            $scope.isRegisterDiv = false;
+        }
+
+        $scope.DonorLoginPost = function (DonorLoginPost) {
+            if (DonorLoginPost.Username == 'Donor' && DonorLoginPost.Password == 'Password') {
+                $('.modal').modal('hide');
+                $('body').removeClass('modal-open');
+                $('.modal-backdrop').remove();
+                $location.path("/donor/login");
+            }
+            else {
+                alert("Invalid Credentials");
+            }
+            $('.modal').modal('hide');
+        }
+
+        $scope.Register = function () {
+            $scope.isRegisterDiv = true;
+            $scope.isReceiverDashboard = false;
+            $scope.isLogin = false;
+        }
+
+
+        $scope.RegisterPost = function (Register) {
+            alert("Registered");
+            $scope.isRegisterDiv = false;
+            $scope.isReceiverDashboard = true;
+            $scope.isLogin = false;
+        }
+
+        $scope.LoginPost = function (LoginModel) {
+            if (LoginModel.Username == 'User' && LoginModel.Password == 'Password') {
+                $('.modal').modal('hide');
+                $('body').removeClass('modal-open');
+                $('.modal-backdrop').remove();
+                $location.path("/receiver/login");
+            }
+            else {
+                alert("Invalid Credentials");
+            }
+            $('.modal').modal('hide');
+        }
+
+
+        $scope.backToDashboardPage = function () {
+            $scope.isRegisterDiv = false;
+            $scope.isReceiverDashboard = true;
+            $scope.isLogin = false;
+            $scope.donorDashboard = true;
+            $scope.isDonorLogin = false;
+        }
+    };
+    app.controller("TitleController", TitleController);
+}());
---
+++ Blood_Bank_New/Blood_Bank_Application/Blood_Bank_Application/Javascripts/app.js
@@ -0,0 +1,29 @@
+﻿// JavaScript source code
+(function () {
+    var app = angular.module("AngularProject", ['ngRoute']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+        .when("/home", {
+            templateUrl: "TitlePage.html",
+            controller: "TitleController"
+        })
+        .when("/receiver", {
+            templateUrl: "ReceiverPage.html",
+            controller: "ReceiverController"
+        })
+        .when("/receiver/login", {
+            templateUrl: "ReceiverLoggedIn.html",
+            controller: "ReceiverController"
+        })
+            .when("/donor/login", {
+                templateUrl: "DonorLoggedIn.html",
+                controller: "DonorController"
+            })
+        .otherwise({ redirectTo: "/home" });
+    });
+
+    app.controller('ReceiverLogged', function ($scope) {
+        $scope.pageClass = 'page-home';
+    });
+}());
